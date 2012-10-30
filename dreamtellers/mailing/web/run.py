a492b2ce0aeff1b865596b0e7536c8c4082b003e
Alberto Valverde <alberto@meteogrid.com>
2012-10-30 16:54:20 +0100

    using paste server in webmailing command

--- dreamtellers/mailing/web/run.py
+++ dreamtellers/mailing/web/run.py
@@ -2,30 +2,18 @@ import sys
 import os.path
 from optparse import OptionParser
 
-from bottle import run
 from paste.deploy.converters import asbool
 
 from sqlalchemy import engine_from_config
 
-from . import app
+from bottle.ext.sqlalchemy import Plugin as SAPlugin
+
 from ..models import metadata
-
-parser = OptionParser()
-parser.add_option('-b', '--bind', dest='bind', default='localhost:8080')
-parser.add_option('-d', '--db', dest='db', default='~/.mailing.db')
-
-def main(args=None):
-    opts = parser.parse_args(args)[0]
-    configure_sqlalchemy(app,
-        {'sqlalchemy.url': 'sqlite:///'+os.path.expanduser(opts.db)}
-        )
-    configure_genshi(app, {'genshi.auto_reload':False})
-    host, port = opts.bind.split(':')
-    run(app, host=host, port=int(port))
+from .template import Plugin as GenshiPlugin
+from . import app
 
 def configure_sqlalchemy(app, config, prefix='sqlalchemy.'):
-    from bottle.ext.sqlalchemy import Plugin
-    plugin = Plugin(
+    plugin = SAPlugin(
         engine_from_config(config, prefix),
         metadata,
         keyword='db',
@@ -35,13 +23,12 @@ def configure_sqlalchemy(app, config, prefix='sqlalchemy.'):
     app.install(plugin)
 
 def configure_genshi(app, config, prefix='genshi.'):
-    from .template import Plugin
     pos = len(prefix)
     config = dict((k[pos:], v) for k,v in config.iteritems()
                   if k.startswith(prefix))
     config['auto_reload'] = asbool(config.get('auto_reload',
                                    config.get('debug', False)))
-    app.install(Plugin())
+    app.install(GenshiPlugin())
 
     
 
@@ -51,6 +38,21 @@ def app_factory(global_config, **local_config):
     configure_sqlalchemy(app, config)
     configure_genshi(app, config)
     return app.wsgi
+
+
+parser = OptionParser()
+parser.add_option('-b', '--bind', dest='bind', default='localhost:8080')
+parser.add_option('-d', '--db', dest='db', default='~/.mailing.db')
+
+def main(args=None):
+    from paste.httpserver import serve
+    opts = parser.parse_args(args)[0]
+    configure_sqlalchemy(app,
+        {'sqlalchemy.url': 'sqlite:///'+os.path.expanduser(opts.db)}
+        )
+    configure_genshi(app, {'genshi.auto_reload':False})
+    host, port = opts.bind.split(':')
+    serve(app, host=host, port=int(port), use_threadpool=True)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
