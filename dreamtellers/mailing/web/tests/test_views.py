0045e51666a8bdd6f8726a50efaba3d8ba928c4d
Alberto Valverde <alberto@meteogrid.com>
2012-11-14 22:34:05 +0100

    updated test

--- dreamtellers/mailing/web/tests/test_views.py
+++ dreamtellers/mailing/web/tests/test_views.py
@@ -3,5 +3,5 @@ from . import BaseViewTest
 
 class TestRootView(BaseViewTest):
     def test_index(self):
-        resp = self.app.get('/')
+        resp = self.app.get('/admin/')
         self.assertIn('<script', resp)
